ed73548991880513289f7da90ce09232616a3022
Akshat13 <161966633+Akshattt4096@users.noreply.github.com>
2025-09-10 15:22:23 +0000

    Data

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //it treats all the js code as a newer version
+ 
+// alert(3+3) // We are inside Node, not browser 
+
+console.log(3
+    +3) //this is not the way to write the code, code readability should be good 
+
+console.log("akshat");
+
+let name = "Hitesh"
+let age = 20
+let isLoggenIn = false
+let address; 
+let weather = null
+
+/* Primitive datatypes 
+number => 2 to the power 53
+bigint
+string => ""
+boolean => true or false
+null => standalone value, representation of null value
+undefined => not defined
+symbol => unique 
+
+
+diff breed=> object datatype
+*/
+console.table([name,age,isLoggenIn,address,weather])
+
+ console.log(typeof address)
+
+ console.log(typeof weather)
