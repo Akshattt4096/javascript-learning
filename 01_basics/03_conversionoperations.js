c97a344766b94a5949588b1e5b04ef1f3a5a0cf4
Akshat13 <161966633+Akshattt4096@users.noreply.github.com>
2025-09-11 14:58:22 +0000

    operations and data conversions

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -30,18 +30,70 @@
 //  onsole.log(typeof stringNumber);
 
  let boolean = true 
- console.log(typeof boolean)
+//  console.log(typeof boolean)
 
  let stringBoolean = String(boolean)
- console.log(stringBoolean)
- console.log(typeof stringBoolean)
-
+//  console.log(stringBoolean)
+//  console.log(typeof stringBoolean)
 
 
   let someName = "Akshat"
-  console.log(typeof someName);
+//   console.log(typeof someName);
 
   let sNumber = Number(someName)
-  console.log(typeof sNumber)
-  console.log(sNumber)
+//   console.log(typeof sNumber)
+//   console.log(sNumber)
+
+
+  // ***********OPERATIONS************
+    let value = 3
+    let negValue = -value;
+    // console.log(negValue)
+
+    let str1 = "hello"
+    let str2 = " Akshat"
+    let str3 = str1+str2
+    // console.log(str3)
+
+    // console.log("1" + 2 + 2);
+    // console.log("1" + 2 );
+    // console.log(1 + "2" + 2);
+    // console.log(1 + 2 + "4");
+
+
+    let num1, num2, num3
+    num1 = num2 = num3 = 2+2 //bad type to assign values code readability is finished 
+    // console.log(num1 + num2 + num3)
+
+    let gameScore= 100
+    gameScore++ 
+    // console.log(gameScore)
+
+    // prefix and postfix operations
+
+    let x = 3
+    const y = x++
+    console.log(`x:${x}, y:${y}`)
+
+    let x1= 4
+    const y1 = ++x1
+    console.log(`x1:${x1}, y:${y1}`);
+
+    let a = 3, b = 2, c = 4
+    let z = ((a++)+(--b)-(c++)+(--a))
+    console.log(z)                      //a=4 b=1 c=5
+    //3 + 1 - 4 + 3 = 3
+    
+
+    
+
+
+
+
+
+
+
+
+
+
   
