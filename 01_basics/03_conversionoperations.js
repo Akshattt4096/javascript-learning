e0a279f7d042f84ac55fbaa97887fe2c1e70d556
Akshat13 <161966633+Akshattt4096@users.noreply.github.com>
2025-09-11 13:32:27 +0000

    datatype conversion

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,37 @@
+//let sore = true
+// onsole.log(typeof(sore));
+ 
+// let valueInNumber = Number(sore)
+// onsole.log(typeof valueInNumber);
+
+// onsole.log(valueInNumber);
+
+// "33" = 33
+// "33ab" => NaN (not a number)
+// null => 0
+// undefined => NaN
+// true => 1; false => 0 
+
+
+// let isLoggedIn = "Akshat"
+//  onsole.log(typeof isLoggedIn);
+
+//  let boolean = Boolean(isLoggedIn)
+// onsole.log(typeof boolean);
+// onsole.log(boolean)
+ 
+ // "akshat" => true 
+ // "" => false
+
+//  let number = 44
+//  onsole.log(typeof number)
+//  let stringNumber = String(number)
+//  onsole.log(stringNumber);
+//  onsole.log(typeof stringNumber);
+
+ let boolean = true 
+ onsole.log(typeof boolean)
+ let stringBoolean = String(boolean)
+ onsole.log(stringBoolean)
+  onsole.log(typeof stringBoolean)
+ 
