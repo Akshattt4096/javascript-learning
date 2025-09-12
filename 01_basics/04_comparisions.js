d588469a802413c81b3047d36cac552646578957
Akshat13 <161966633+Akshattt4096@users.noreply.github.com>
2025-09-12 14:56:19 +0000

    comparison operator

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log(2!=1)
+
+// console.log("2" > 1)
+// console.log("5" > true)
+// console.log(true > false)
+// console.log("5" > "hello")
+
+console.log(null >= 0)
+console.log(null === 0) // When equality constraints are used it           doesnt convert null into a number but if Comparision operators are used it will convert the null into a number which is 0
+console.log(null < 0)
+
+
+
+/// === strict check it will show that both datatypes are different and it cannot be compared 
+
+console.log("2" === 2)
+
