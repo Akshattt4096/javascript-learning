fc50babf564ae70dd056a536d7c1f656b3f17c6c
Akshat13 <161966633+Akshattt4096@users.noreply.github.com>
2025-09-15 15:35:00 +0000

    string methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,36 @@
+const name = "hello"
+const anotherName = "world"
+//console.log(name +  anotherName)
+
+const gameName = new String ("akshat-gh-ok")
+
+//console.log(`My first word is ${name} and my second word is ${anotherName}`)
+
+//console.log(gameName[0])
+//console.log(gameName.__proto__)
+//console.log(gameName.charAt(2))
+//console.log(gameName.indexOf('t'))
+//console.log(gameName.endsWith('h'))
+//console.log(gameName.length)
+//console.log (gameName.toUpperCase())
+// const capitalizedSecond = 
+//     gameName[0] + gameName[1].toUpperCase() + gameName.slice(2)
+//     console.log(capitalizedSecond)
+//console.log(gameName)
+//console.log(gameName.trim())
+// const someName = gameName.substring(0, 4)
+// console.log(someName)
+
+
+//console.log(gameName.substring(0,4))
+// console.log(gameName.slice(-7,4)) // it will extract the value on index 4,5,6
+
+
+const url = "https://akshat.com/akshat%20ghadigaonkar"
+console.log(url.replace('%20','-'))
+console.log(gameName.split('-'))
+
+
+// console.log(gameName.toString())
+// console.log(gameName.valueOf())
+
