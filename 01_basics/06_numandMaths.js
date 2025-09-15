2580a9efee9b38d4d816b9416a88511e90683ead
Akshat13 <161966633+Akshattt4096@users.noreply.github.com>
2025-09-15 16:02:12 +0000

    number methods

---
+++ 01_basics/06_numandMaths.js
@@ -0,0 +1,17 @@
+const score = 100
+console.log(score)
+const count =  new Number(400)
+console.log(count)
+
+// console.log(count.toString().length)
+// console.log(count.toFixed(2))
+
+const localString = 100000
+console.log(localString.toLocaleString('en-IN'))
+
+const approx = 123.7865
+console.log(approx.toPrecision(2))
+console.log(approx.toExponential())
+
+
+// ---------MATHS----------
